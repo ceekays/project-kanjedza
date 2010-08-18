5d05c2c87c5dbf0eb50b89f0589db27dd5c63726
ceekays <kachaleedmond@gmail.com>
2010-08-18 14:16:23 +0200

    Added a line that helps the .prawn template respond to pdf format

--- inpatient/app/controllers/patients_controller.rb
+++ inpatient/app/controllers/patients_controller.rb
@@ -7,6 +7,7 @@ class PatientsController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @patients }
+      format.pdf { render :layout => false }
     end
   end
 
